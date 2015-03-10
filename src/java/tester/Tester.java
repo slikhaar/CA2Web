02a6969988e297d36a82e8d820aae3f3c85bd5f9
Afrooz <Afrooz@10.50.137.59>
2015-03-10 09:13:18 +0100

    10-03-2015
    Entity-klasser + facade: createPerson + getPersons + addHobby + addHobbyFromId
    
    Driver + jars

--- src/java/tester/Tester.java
+++ src/java/tester/Tester.java
@@ -5,10 +5,36 @@
  */
 package tester;
 
+import java.util.List;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Afrooz
  */
 public class Tester {
-    
+
+//    ProjectFacade facade = new ProjectFacade(Persistence.createEntityManagerFactory("StudyPoint4DemoPU"));
+//    Project p = new Project("Projekt 1");
+//    p  = facade.createProject(p);
+//
+//    System.out.println (p.getId
+//    ());
+//        
+//        Task t1 = new Task("Opgave 1");
+//    p  = facade.addTask(p, t1);
+//
+//    System.out.println (p.getTasks
+//    ().get(0).getName());
+//        
+//        Task t2 = new Task("Opgave 2");
+//    p  = facade.addTaskFromId(1, t2);
+//
+//    System.out.println (p.getTasks
+//    ().get(1).getName());
+//        
+//        List<Project> projects = facade.getProjects();
+//        System.out.println(projects.size());
+//        System.out.println(projects.get(0).getName();
+
 }
